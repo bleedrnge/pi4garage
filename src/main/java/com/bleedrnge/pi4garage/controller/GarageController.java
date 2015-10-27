390c54884ec4c2bb27441d77862c8b4a06d50865
bleedrnge <bleedrnge@gmail.com>
2015-10-26 21:46:10 -0400

    Initial commit

---
+++ src/main/java/com/bleedrnge/pi4garage/controller/GarageController.java
@@ -0,0 +1,59 @@
+package com.bleedrnge.pi4garage.controller;
+
+import com.pi4j.io.gpio.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GarageController {
+
+    private static GpioPinDigitalOutput lightPin;
+    private static GpioPinDigitalOutput doorPin;
+    private static GpioPinDigitalInput sensorPin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
+    }
+
+    @RequestMapping("/light")
+    public String light() {
+        if (lightPin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            lightPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLights", PinState.LOW);
+        }
+
+        lightPin.pulse(5);
+
+        return "OK";
+    }
+
+    @RequestMapping("/door")
+    public String door() {
+        if (doorPin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            doorPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyDoor", PinState.LOW);
+        }
+
+        doorPin.pulse(5);
+
+        return "OK";
+    }
+
+    @RequestMapping("/sensor")
+    public String doorSensor() {
+        if (sensorPin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            sensorPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "MyDoor", PinPullResistance.PULL_DOWN);
+        }
+
+        if(sensorPin.isHigh()) {
+            return "Open";
+        }
+        else{
+            return "Closed";
+        }
+    }
+
+}
